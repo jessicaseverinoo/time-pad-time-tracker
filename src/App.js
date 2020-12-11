b1b3da446c7bc5b41a30eaee274addf5ad1ea9c3
Jessica Severino <jessica.severino@accurate.com.br>
2020-12-11 14:25:35 -0300

    (feat): adiciona styled component no projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import styled from 'styled-components';
+
 function App() {
+  // Create a Title component that'll render an <h1> tag with some styles
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  `;
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Wrapper>
+        <Title>
+          Hello World!
+        </Title>
+      </Wrapper>
       </header>
     </div>
   );
